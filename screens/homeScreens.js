2eae6c60f64ead009c8dc88b1777bae060dfc56c
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2023-03-30 19:47:54 +0300

    .

---
+++ screens/homeScreens.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const homeScreens = () => {
+  return (
+    <View>
+      <Text>homeScreens</Text>
+    </View>
+  )
+}
+
+export default homeScreens
