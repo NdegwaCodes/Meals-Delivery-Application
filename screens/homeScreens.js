2fbd15651c6ce3e7a2a66cbdbcbddd4660dd10d6
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2023-03-30 22:38:56 +0300

    Update homeScreens.js

--- screens/homeScreens.js
+++ screens/homeScreens.js
@@ -1,11 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const homeScreens = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+       
+    }, []);
   return (
-    <View>
-      <Text>homeScreens</Text>
-    </View>
+    <SafeAreaView>
+      <Text className= "text-red-600">
+      {/*Header */}
+      <View>
+        <image source{{
+            url: 'https://links.papareact.com/wru',
+        }}
+        className= "h-7 w-y bg-gray-700 p-4 rounded-full"
+        />
+        <View>
+        <Text></Text>
+        </View>
+      </View>
+      </Text>
+    </SafeAreaView>
   )
 }
 
