503fdc394b517136378d2941a375d26c8048996c
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2023-04-03 06:36:02 +0300

    Updates

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import { ScrollView } from 'react-native-web'
 
 const FeaturedRow = ({id, title, description,}) => {
   return (
@@ -10,6 +11,28 @@ const FeaturedRow = ({id, title, description,}) => {
       <ArrowRightIcon color="#00CCBB" />
     </View>
     <Text className="text-xs text-gray-500 px-4">(description)</Text>
+    <ScrollView 
+    horizontal
+    contentContainerStyle={{
+        paddingHorizontal: 15,
+    }}
+    showsHorizontalScrollIndicator={false}
+    className="pt-4"
+    >
+    {/*Grocery Cards */}  
+      <RestaurantCard 
+      id={123}
+      imgUrl="https://links.papareact.com/gn7"
+        title="Sukuma Wiki"
+        rating={4.5}
+        genre="Kenyan"
+        address="Nairobi"
+        short_description="Sukuma wiki is a Kenyan dish made from collard greens, tomatoes, onions, and spices. It is a popular dish in Kenya and is often served with ugali, chapati, or rice."
+        dishes={[]}
+        long={36.8219}
+        lat={-1.2921}
+      />
+    </ScrollView>
     </View>
   )
 }
