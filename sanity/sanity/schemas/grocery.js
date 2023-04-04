6bc2c2e6148c298b24b66c6471f11adec977951f
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2023-04-04 18:40:39 +0300

    Updates

--- sanity/sanity/schemas/grocery.js
+++ sanity/sanity/schemas/grocery.js
@@ -1,15 +1,16 @@
-export default defineType({
-  name: 'name',
-  type: 'string',
-  title: 'Grocery Shop Name',
-  validation: Rule => Rule.required(),
+export default {
+  name: "name",
+  title: "Grocery Shop Name",
+  type: "document",
   fields: [
-    { name: 'title', title: 'Title', type: 'string' },
-    { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } },
-    { name: 'author', title: 'Author', type: 'reference', to: { type: 'author' } },
-    { name: 'mainImage', title: 'Main image', type: 'image', options: { hotspot: true } },
-    { name: 'categories', title: 'Categories', type: 'array', of: [{ type: 'reference', to: { type: 'category' } }] },
-    { name: 'publishedAt', title: 'Published at', type: 'datetime' },
-    { name: 'body', title: 'Body', type: 'blockContent' }
-  ]
-})
+    { name: "title", title: "Title", type: "string",  validation: (Rule) => Rule.required(), },
+    { name: "short_description", title: "Short Description", type: "string",validation: (Rule) => Rule.max(200),},
+    { name: "image", title: "Image of the Grocery Shop", type: "image",},
+    { name: "lat", title: "Latitude of the Restaurant", type: "number",},
+    { name: "long", title: "Longitude of the Restaurant", type: "number", },
+    { name: "address", title: "Grocery Shop address", type: "string",  validation: (Rule) => Rule.required(), },
+    { name: "body", title: "Enter a Rating from (1-5 Stars)", type: "number",  validation: (Rule) => Rule.required().min(1).max(5).error("Please enter a Value between 1 and 5",), },
+    { name: "type", title: "Category", validation: (Rule) => Rule.required(), type: "reference", to: [{ type: "category"}], },
+    { name: "dishes", title: "Dishes", type: "array", of: [{ type: "reference", to: [{type: "dish"}]}] },
+  ],
+};
